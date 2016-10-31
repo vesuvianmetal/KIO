1432791f2acc1c8739fff90a634f1d0dc540e925
Penudo McFly <Penudo McFly@DESKTOP-R3G2O88>
2016-10-31 11:01:44 -0700

    new commit

--- src/Conexion_BD.java
+++ src/Conexion_BD.java
@@ -1,35 +1,23 @@
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import javax.swing.JOptionPane;
 import java.sql.*;
-import javax.swing.*;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Penudo McFly
- */
 public class Conexion_BD {
-   Connection conn = null;
-   public static Connection conector(){
-       
-       try {
-           Class.forName("org.sqlite.JDBC");
-           Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Samuel\\Documents\\NetBeansProjects\\Elecciones\\Elecciones.sqlite");
-      JOptionPane.showMessageDialog(null, "Conexion Establecida");
-           return conn;
-       }
-       catch (Exception e){
-           JOptionPane.showMessageDialog(null, e);
-           return null;
-       }
+   private static Connection cnx = null;
+   public static Connection obtener() throws SQLException, ClassNotFoundException {
+      if (cnx == null) {
+         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection("jdbc:mysql://localhost/java_mysql", "root", "");
+         } catch (SQLException ex) {
+            throw new SQLException(ex);
+         } catch (ClassNotFoundException ex) {
+            throw new ClassCastException(ex.getMessage());
+         }
+      }
+      return cnx;
    }
-    
-   
+   public static void cerrar() throws SQLException {
+      if (cnx != null) {
+         cnx.close();
+      }
+   }
 }
-
