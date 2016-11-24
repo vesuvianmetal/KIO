73f656c5828513d9b971c0c54c3f57e257b65305
Disco <Disco@Discouno>
2016-11-24 01:51:35 -0700

    fondo

--- src/Interfaz_Lenguajes.java
+++ src/Interfaz_Lenguajes.java
@@ -29,10 +29,11 @@ public class Interfaz_Lenguajes extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        titulo = new javax.swing.JLabel();
         tablavalores = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         panebuscar = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        areabucar = new javax.swing.JLabel();
         paneditar = new javax.swing.JPanel();
         areaeditar = new javax.swing.JLabel();
         panelagregar = new javax.swing.JPanel();
@@ -47,6 +48,11 @@ public class Interfaz_Lenguajes extends javax.swing.JFrame {
             }
         });
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        titulo.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
+        titulo.setForeground(new java.awt.Color(255, 255, 255));
+        titulo.setText("Centro de Idiomas");
+        getContentPane().add(titulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 10, -1, -1));
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -63,7 +69,7 @@ public class Interfaz_Lenguajes extends javax.swing.JFrame {
         ));
         tablavalores.setViewportView(jTable1);
 
-        getContentPane().add(tablavalores, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 80, 520, 110));
+        getContentPane().add(tablavalores, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 90, 520, 110));
 
         panebuscar.setBackground(new java.awt.Color(0, 204, 204));
         panebuscar.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 204, 204), 1, true));
@@ -81,8 +87,8 @@ public class Interfaz_Lenguajes extends javax.swing.JFrame {
 
         getContentPane().add(panebuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 210, 20));
 
-        jLabel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 153, 153), 2, true));
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 210, 170));
+        areabucar.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 153, 153), 2, true));
+        getContentPane().add(areabucar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 210, 170));
 
         paneditar.setBackground(new java.awt.Color(0, 204, 204));
         paneditar.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 204, 204), 1, true));
@@ -174,13 +180,14 @@ int a=JOptionPane.showConfirmDialog(null,"Está Seguro Que Deseea Salir?");
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel areaagregar;
+    private javax.swing.JLabel areabucar;
     private javax.swing.JLabel areaeditar;
     private javax.swing.JLabel fondo;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JTable jTable1;
     private javax.swing.JPanel panebuscar;
     private javax.swing.JPanel paneditar;
     private javax.swing.JPanel panelagregar;
     private javax.swing.JScrollPane tablavalores;
+    private javax.swing.JLabel titulo;
     // End of variables declaration//GEN-END:variables
 }
