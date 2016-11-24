aee6c2ed957601283183af3dc7f1b796a2deae62
Disco <Disco@Discouno>
2016-11-24 01:49:37 -0700

    fondo

--- src/Interfaz_Lenguajes.java
+++ src/Interfaz_Lenguajes.java
@@ -31,8 +31,12 @@ public class Interfaz_Lenguajes extends javax.swing.JFrame {
 
         tablavalores = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        panebuscar = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        paneditar = new javax.swing.JPanel();
         areaeditar = new javax.swing.JLabel();
+        panelagregar = new javax.swing.JPanel();
+        areaagregar = new javax.swing.JLabel();
         fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
@@ -59,13 +63,64 @@ public class Interfaz_Lenguajes extends javax.swing.JFrame {
         ));
         tablavalores.setViewportView(jTable1);
 
-        getContentPane().add(tablavalores, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 100, 480, 110));
+        getContentPane().add(tablavalores, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 80, 520, 110));
+
+        panebuscar.setBackground(new java.awt.Color(0, 204, 204));
+        panebuscar.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 204, 204), 1, true));
+
+        javax.swing.GroupLayout panebuscarLayout = new javax.swing.GroupLayout(panebuscar);
+        panebuscar.setLayout(panebuscarLayout);
+        panebuscarLayout.setHorizontalGroup(
+            panebuscarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 208, Short.MAX_VALUE)
+        );
+        panebuscarLayout.setVerticalGroup(
+            panebuscarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 18, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(panebuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 210, 20));
 
         jLabel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 153, 153), 2, true));
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 30, 190, 190));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 210, 170));
+
+        paneditar.setBackground(new java.awt.Color(0, 204, 204));
+        paneditar.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 204, 204), 1, true));
+
+        javax.swing.GroupLayout paneditarLayout = new javax.swing.GroupLayout(paneditar);
+        paneditar.setLayout(paneditarLayout);
+        paneditarLayout.setHorizontalGroup(
+            paneditarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 518, Short.MAX_VALUE)
+        );
+        paneditarLayout.setVerticalGroup(
+            paneditarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 28, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(paneditar, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 210, 520, 30));
 
         areaeditar.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 153, 153), 2, true));
-        getContentPane().add(areaeditar, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 240, 480, 230));
+        getContentPane().add(areaeditar, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 240, 520, 240));
+
+        panelagregar.setBackground(new java.awt.Color(0, 204, 204));
+        panelagregar.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 204, 204), 2, true));
+
+        javax.swing.GroupLayout panelagregarLayout = new javax.swing.GroupLayout(panelagregar);
+        panelagregar.setLayout(panelagregarLayout);
+        panelagregarLayout.setHorizontalGroup(
+            panelagregarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 206, Short.MAX_VALUE)
+        );
+        panelagregarLayout.setVerticalGroup(
+            panelagregarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 26, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(panelagregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 210, 210, 30));
+
+        areaagregar.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 153, 153), 2, true));
+        getContentPane().add(areaagregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 240, 210, 240));
 
         fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Utilizables/interfazbiblioteca.jpg"))); // NOI18N
         getContentPane().add(fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -118,10 +173,14 @@ int a=JOptionPane.showConfirmDialog(null,"Está Seguro Que Deseea Salir?");
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel areaagregar;
     private javax.swing.JLabel areaeditar;
     private javax.swing.JLabel fondo;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JPanel panebuscar;
+    private javax.swing.JPanel paneditar;
+    private javax.swing.JPanel panelagregar;
     private javax.swing.JScrollPane tablavalores;
     // End of variables declaration//GEN-END:variables
 }
